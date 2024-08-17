0565ba2b025696a78616fdfeabc45802697177c2
zeelweetech <zeel.weetech@gmail.com>
2024-08-17 09:45:38 +0530

    route setup"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./views/pages/login/Login";
-import Register from "./views/pages/register/Register";
 import DefaultLayout from "./layout/DefaultLayout";
-import HomePage from "./views/homepage";
 
 function App() {
   return (
@@ -23,13 +19,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route>
-            <Route exact path="/login" name="Login Page" element={<Login />} />
-            <Route
-              exact
-              path="/register"
-              name="Register Page"
-              element={<Register />}
-            />
             <Route path="*" name="Home" element={<DefaultLayout />} />
           </Route>
         </Routes>
