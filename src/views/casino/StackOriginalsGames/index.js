747034e73b2c82be136ecdb4ba1179d2e56b725c
zeelweetech <zeel.weetech@gmail.com>
2024-10-17 17:56:13 +0530

    path change

--- src/views/casino/StackOriginalsGames/index.js
+++ src/views/casino/StackOriginalsGames/index.js
@@ -16,7 +16,7 @@ function GameContainer() {
         return <CrashGame />;
       case "Plinko":
         return <PlinkoGame />;
-      case "Miens":
+      case "Mines":
         return <MinesGame />;
       case "Limbo":
         return <LimboGame />;
