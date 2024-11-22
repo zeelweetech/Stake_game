9fd281588f6034fd57977a5b54b73b22e2745187
Savan-Hilance <Savan@hilance.app>
2024-11-22 17:38:53 +0530

    fix sidebar

---
+++ src/views/component/ComeSoon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { RotatingLines } from "react-loader-spinner";
+import GameFooter from "./GameFooter";
+
+function ComeSoon() {
+  return (
+  <>
+   <h1 className="flex justify-center h-screen items-center text-white text-5xl">Coming Soon...</h1>
+   <GameFooter />
+  </>
+  );
+}
+
+export default ComeSoon;
