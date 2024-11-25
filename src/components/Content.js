408559edf1cae63e7ebee18a17ae9ba856813944
SAVAN ITALIYA <savan@hilance.app>
2024-11-25 10:04:58 +0530

    footer

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import routes from "../routes";
 import Loader from "../views/component/Loader";
+import Footer from "./Footer";
 
 function Content() {
   return (
